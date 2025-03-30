e101e587ee0f9acb86f26f37546f48fa284b0379
EliaquimJorras <eliaquimjorras@gmail.com>
2025-03-29 22:23:09 -0300

    feat: adicionado estrutura de banco de dados da tabela Beneficiarios

---
+++ FI.AtividadeEntrevista/BLL/BoBeneficiario.cs
@@ -0,0 +1,23 @@
+﻿using FI.AtividadeEntrevista.DAL.Beneficiarios;
+using FI.AtividadeEntrevista.DML;
+using System.Collections.Generic;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoBeneficiario
+    {
+        public long Incluir(Beneficiario beneficiario)
+        {
+            DaoBeneficiario beneficiarioDao = new DaoBeneficiario();
+
+            return beneficiarioDao.Incluir(beneficiario);
+        }
+
+        public List<Beneficiario> ListarBeneficiarios(long idCliente)
+        {
+            DaoBeneficiario beneficiarioDao = new DaoBeneficiario();
+
+            return beneficiarioDao.ListarBeneficiarios(idCliente);
+        }
+    }
+}
---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/DaoBeneficiario.cs
@@ -0,0 +1,55 @@
+﻿using FI.AtividadeEntrevista.DML;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace FI.AtividadeEntrevista.DAL.Beneficiarios
+{
+    internal class DaoBeneficiario : AcessoDados
+    {
+        internal long Incluir(Beneficiario beneficiario)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>
+            {
+                new SqlParameter("Nome", beneficiario.Nome),
+                new SqlParameter("CPF", beneficiario.CPF),
+                new SqlParameter("IdCliente", beneficiario.IdCliente)
+            };
+
+            DataSet ds = base.Consultar("FI_SP_IncBeneficiario", parametros);
+
+            long ret = 0;
+
+            if (ds.Tables[0].Rows.Count > 0)
+                long.TryParse(ds.Tables[0].Rows[0][0].ToString(), out ret);
+
+            return ret;
+        }
+
+        internal List<Beneficiario> ListarBeneficiarios(long idCliente)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter> { new SqlParameter("IdCliente", idCliente) };
+
+            DataSet ds = base.Consultar("FI_SP_ListarBeneficiarios", parametros);
+
+            List<Beneficiario> beneficiarios = Converter(ds);
+
+            return beneficiarios;
+        }
+
+        private List<Beneficiario> Converter(DataSet ds)
+        {
+            if (ds?.Tables == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
+                return new List<Beneficiario>();
+
+            return ds.Tables[0].AsEnumerable().Select(row => new Beneficiario
+            {
+                Id = row.Field<long>("Id"),
+                Nome = row.Field<string>("Nome"),
+                CPF = row.Field<string>("CPF"),
+                IdCliente = row.Field<long>("IdCliente")
+            }).ToList();
+        }
+    }
+}
