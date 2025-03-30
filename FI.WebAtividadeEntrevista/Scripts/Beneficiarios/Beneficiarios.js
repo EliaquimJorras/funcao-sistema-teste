2b5e09149ff3e99c211f3ebba65d55734bee7c78
EliaquimJorras <eliaquimjorras@gmail.com>
2025-03-30 15:19:02 -0300

    feat: criação gestão base de beneficiario no javascript e melhorado layout do pop-up

--- FI.WebAtividadeEntrevista/Scripts/Beneficiarios/Beneficiarios.js
+++ FI.WebAtividadeEntrevista/Scripts/Beneficiarios/Beneficiarios.js
@@ -1,4 +1,34 @@
-﻿$(document).ready(function () {
+﻿let BENEFICIARIO_LIST = []
+
+$(document).ready(function () {
+    MostrarPopUp();
+
+    // fechar modal
+    $(document).on('hidden.bs.modal', '#modalBeneficiarios', function () {
+        $('#beneficiario-modal').focus();
+
+        $('.modal-backdrop').remove();
+    });
+
+    // excluir beneficiario
+    $(document).on("click", ".excluir-button", function (e) {
+        e.preventDefault();
+
+        let indexBeneficiario = Number($(this).data("index"));
+
+        if (isNaN(indexBeneficiario) || indexBeneficiario < 0 || indexBeneficiario >= BENEFICIARIO_LIST.length)
+            return;
+
+        BENEFICIARIO_LIST.splice(indexBeneficiario, 1);
+
+        localStorage.setItem("beneficiario-list", JSON.stringify(BENEFICIARIO_LIST));
+
+        $("table.table tbody").empty();
+        PreencherListaBeneficiarios();
+    });
+});
+
+function MostrarPopUp() {
     $("#beneficiario-modal").on("click", function (e) {
         e.preventDefault();
 
@@ -18,17 +48,100 @@
                 if (response.Status === undefined) {
                     $('#modal-container').html(response);
                     $('#modalBeneficiarios').modal('show');
-                    $('#cpf').mask('000.000.000-00');
+                    $('#cpf-modal').mask('000.000.000-00');
+
+                    PreencherListaBeneficiarios();
+                    IncluirBeneficiario();
                 } else {
                     console.error('Erro na resposta:', response);
                 }
             }
         });
     });
+}
 
-    $(document).on('hidden.bs.modal', '#modalBeneficiarios', function () {
-        $('#beneficiario-modal').focus();
+function IncluirBeneficiario() {
+    $("#incluir-button").on("click", function (e) {
+        e.preventDefault();
 
-        $('.modal-backdrop').remove();
+        let cpfBeneficiario = $("#cpf-modal").val()
+        let nomeBeneficiario = $("#nome-modal").val()
+
+        if (ValidarCampos(cpfBeneficiario, nomeBeneficiario))
+            return;
+
+        BENEFICIARIO_LIST.push({
+            'CPF': cpfBeneficiario,
+            'NOME': nomeBeneficiario
+        });
+
+        localStorage.setItem("beneficiario-list", JSON.stringify(BENEFICIARIO_LIST));
+
+        AdicionarLinha(cpfBeneficiario, nomeBeneficiario, BENEFICIARIO_LIST.length - 1)
+
+        $("#cpf-modal").val("")
+        $("#nome-modal").val("")
     });
-});
+}
+
+function ValidarCampos(cpf, nome) {
+    let mensagemErro = "";
+
+    if (cpf != "" && nome != "") {
+        if (BENEFICIARIO_LIST.find(item => item.CPF === cpf))
+            mensagemErro = "O CPF informado já foi cadastrado como beneficiário";
+        else if (cpf.length != 14)
+            mensagemErro = "O CPF informado está incompleto";
+        else
+            return false;
+    }
+
+    if (cpf == "")
+        mensagemErro = "O CPF é obrigatório";
+
+    if (nome == "")
+        mensagemErro += "<br> O Nome é obrigatório";
+
+    ModalDialog("Campo(s) Inválido(s)", mensagemErro);
+
+    $('#beneficiario-modal').focus();
+
+    return true;
+}
+
+function AdicionarLinha(cpf, nome, index) {
+    let novaLinha = `
+        <tr>
+            <td data-label="CPF">${cpf}</td>
+            <td class="nome-cell" data-label="Nome" title="${nome}">${nome}</td>
+            <td data-label="Ações" style="justify-content: space-between; display: flex;">
+                <button data-index="${index}" class="btn btn-primary">Alterar</button>
+                <button data-index="${index}" class="btn btn-primary excluir-button">Excluir</button>
+            </td>
+        </tr>
+    `;
+
+    const $table = $("table.table");
+    const $tbody = $table.find("tbody");
+
+    $tbody.append(novaLinha);
+
+    if ($table.hasClass("hidden"))
+        $table.removeClass("hidden");
+}
+
+function PreencherListaBeneficiarios() {
+    let beneficiarioList = localStorage.getItem("beneficiario-list")
+
+    if (beneficiarioList) {
+        const parsedList = JSON.parse(beneficiarioList)
+
+        if (Array.isArray(parsedList) && parsedList.length > 0) {
+            parsedList.forEach((item, index) => {
+                AdicionarLinha(item.CPF, item.NOME, index)
+            })
+
+            BENEFICIARIO_LIST = parsedList;
+        }
+    }
+}
